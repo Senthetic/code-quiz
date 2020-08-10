15e5d2e7c545c6a1c164d21e0eff9bbc186171df
Andrew Collier <atc3598@icloud.com>
2020-08-09 22:26:49 -0400

    Functioning code

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
-// On click event on the start button\
-var startButton = document.querySelector("#start-timer");
 var timeEl = document.querySelector("time");
 var mainEl = document.getElementById("main");
 var startButton = document.getElementById("startButton");
 var timer = document.getElementById("remaining-time");
 var responses = document.getElementById("answers");
 var testscreen = document.getElementById("quiz-screen");
-var questionNum = 0;
-var quizDone = false;
-var score = 0;
-var secondsLeft = 60;
+var results = document.getElementById("highscore");
 
-var questions = [
-  {
-    text: "What is javascript?",
-    answers: ["what", "no clue", "still learning", "everything"],
-    rightAnswer: "everything",
-  },
-  {
-    text: "What is css?",
-    answers: [
-      "no clue",
-      "where am i",
-      "sir im still learning",
-      "its everything",
-    ],
-    rightAnswer: "its everything",
-  },
-  {
-    text: "What is sshkey?",
-    answers: ["whattttt", "no clueeeee", "still learningggg", "everythingggg"],
-    rightAnswer: "everythingggg",
-  },
-  {
-    text: "What is a node?",
-    answers: ["what", "no clue", "still learning", "everything"],
-    rightAnswer: "everything",
-  },
-  {
-    text: "What is an element?",
-    answers: ["who", "why", "is what", "element"],
-    rightAnswer: "element",
-  },
-];
-//timer function, subtracts time on wrong answer, game ends at 0 setInterval
+var questionsArray = [{
+  question: "Commonly used data types don't include:",
+  answers: ["Strings", "booleans", "alerts", "alerts"],
+  rightAnswer: "Strings",
+},
+{
+  question: "the condition in an If/else statement is enclose within",
+  answers: ["quotes", "parentheses", "curly brackets", "square bracket"],
+  rightAnswer: "Curly brackets",
+}, {
+  question: "Arrays in Javascript can be use to store",
+  answers: ["numbers and strings", "other array", "booleans", "all above"],
+  rightAnswer: "all above",
+}, {
+  question: "string values must be enclosed within -----------when being assigned to variables.",
+  answers: ["commas", "curly braces", "quotes", "parentheses"],
+  rightAnswer: "quotes",
+}, {
+  question: "a very useful tool used during development and debugging for printing content to the debuger is",
+  answers: ["Javascript", "terminal/bash", "for loops", "console log"],
+  rightAnswer: "console log",
+}];
 console.log(timer)
 
 var currentQuestionIndex = 0;
 var userScore = 0;
-var secondsLeft = "75";
+var secondsLeft = "80";
+
+function displayResult(){
+  testscreen.innerHTML = "";
+  var newEl = document.createElement("h4");
+  newEl.textContent = "You finished with " + userScore + " correct!";
+  testscreen.append(newEl);
+
+  var restart = document.createElement("button");
+  restart.textContent = "Restart?";
+  testscreen.append(restart);
+  restart.addEventListener("click", function (event) {
+    currentQuestionIndex = 0;
+    secondsLeft = 80;
+    console.log(event.target);
+    displayQuestions();
+
+})
+}
 
 function startTime() {
   var timerInterval = setInterval(function () {
@@ -64,13 +66,14 @@ function startTime() {
 }
 ;
 function displayQuestions() {
-  var currentQuestion = questions[currentQuestionIndex];
+  var currentQuestion = questionsArray[currentQuestionIndex];
   startButton.remove();
   testscreen.innerHTML = "";
   var newEl = document.createElement("h4");
   newEl.textContent = currentQuestion.question;
   testscreen.append(newEl);
-  for (var i = 0; i < currentQuestion.answers.length; i++) {
+  var i = 0;
+  for (i = 0; i < currentQuestion.answers.length; i++) {
     var answersElement = document.createElement("button");
 
   answersElement.setAttribute("class", "answers-i");
@@ -88,13 +91,26 @@ function displayQuestions() {
 
       alert("correct");
       userScore++;
+  results.textContent = "Score: " + userScore;
+  testscreen.append(results);
     } else {
       secondsLeft = secondsLeft - 10;
       alert("incorrect");
     }
     console.log(userScore);
   })
-}}
+}
+// if(i===currentQuestionIndex){
+// var resultEl = document.createElement("h4");
+//   resultEl.textContent = "Nice, you got " + userScore + " correct";
+//   testscreen.append(resultEl);
+// }
+if(currentQuestionIndex===questionsArray.length-1){
+  displayResult();
+
+}
+
+}
 startButton.addEventListener("click", function () {
   displayQuestions();
   startTime()
