cb43987bd91c772ac86b1fe41bc71444a169d2ab
Andrew Collier <atc3598@icloud.com>
2020-08-09 22:36:47 -0400

    added final touches

--- script.js
+++ script.js
@@ -4,41 +4,54 @@ var startButton = document.getElementById("startButton");
 var timer = document.getElementById("remaining-time");
 var responses = document.getElementById("answers");
 var testscreen = document.getElementById("quiz-screen");
-var results = document.getElementById("highscore");
+var results = document.getElementById("score");
+var highscore = document.getElementById("high-score");
 
-var questionsArray = [{
-  question: "Commonly used data types don't include:",
-  answers: ["Strings", "booleans", "alerts", "alerts"],
-  rightAnswer: "Strings",
-},
-{
-  question: "the condition in an If/else statement is enclose within",
-  answers: ["quotes", "parentheses", "curly brackets", "square bracket"],
-  rightAnswer: "Curly brackets",
-}, {
-  question: "Arrays in Javascript can be use to store",
-  answers: ["numbers and strings", "other array", "booleans", "all above"],
-  rightAnswer: "all above",
-}, {
-  question: "string values must be enclosed within -----------when being assigned to variables.",
-  answers: ["commas", "curly braces", "quotes", "parentheses"],
-  rightAnswer: "quotes",
-}, {
-  question: "a very useful tool used during development and debugging for printing content to the debuger is",
-  answers: ["Javascript", "terminal/bash", "for loops", "console log"],
-  rightAnswer: "console log",
-}];
-console.log(timer)
+var questionsArray = [
+  {
+    question: "Commonly used data types don't include:",
+    answers: ["Strings", "booleans", "number", "alerts"],
+    rightAnswer: "alerts",
+  },
+  {
+    question: "the condition in an If/else statement is enclose within",
+    answers: ["quotes", "parentheses", "curly brackets", "square bracket"],
+    rightAnswer: "Curly brackets",
+  },
+  {
+    question: "Arrays in Javascript can be use to store",
+    answers: ["numbers and strings", "other array", "booleans", "all above"],
+    rightAnswer: "all above",
+  },
+  {
+    question:
+      "string values must be enclosed within -----------when being assigned to variables.",
+    answers: ["commas", "curly braces", "quotes", "parentheses"],
+    rightAnswer: "quotes",
+  },
+  {
+    question:
+      "a very useful tool used during development and debugging for printing content to the debuger is",
+    answers: ["Javascript", "terminal/bash", "for loops", "console log"],
+    rightAnswer: "console log",
+  },
+];
+console.log(timer);
 
 var currentQuestionIndex = 0;
 var userScore = 0;
 var secondsLeft = "80";
+var hiScore = 0;
 
-function displayResult(){
+function displayResult() {
   testscreen.innerHTML = "";
   var newEl = document.createElement("h4");
   newEl.textContent = "You finished with " + userScore + " correct!";
   testscreen.append(newEl);
+  if (hiScore < userScore) {
+    hiScore = userScore;
+  }
+  highscore.textContent = "Highscore: " + hiScore;
 
   var restart = document.createElement("button");
   restart.textContent = "Restart?";
@@ -48,8 +61,7 @@ function displayResult(){
     secondsLeft = 80;
     console.log(event.target);
     displayQuestions();
-
-})
+  });
 }
 
 function startTime() {
@@ -64,7 +76,6 @@ function startTime() {
     }
   }, 1000);
 }
-;
 function displayQuestions() {
   var currentQuestion = questionsArray[currentQuestionIndex];
   startButton.remove();
@@ -76,42 +87,39 @@ function displayQuestions() {
   for (i = 0; i < currentQuestion.answers.length; i++) {
     var answersElement = document.createElement("button");
 
-  answersElement.setAttribute("class", "answers-i");
-  answersElement.setAttribute("class", "button btn-primary m-5 rounded-pill");
-  answersElement.setAttribute("data-index", i);
-  var quizAnswers = currentQuestion.answers[i];
-  answersElement.textContent = quizAnswers;
-  testscreen.append(answersElement);
-  answersElement.addEventListener("click", function (event) {
-    currentQuestionIndex++;
-    console.log(event.target);
-    displayQuestions();
-    var userchoice = event.target.textContent;
-    if (userchoice === currentQuestion.rightAnswer) {
-
-      alert("correct");
-      userScore++;
-  results.textContent = "Score: " + userScore;
-  testscreen.append(results);
-    } else {
-      secondsLeft = secondsLeft - 10;
-      alert("incorrect");
-    }
-    console.log(userScore);
-  })
-}
-// if(i===currentQuestionIndex){
-// var resultEl = document.createElement("h4");
-//   resultEl.textContent = "Nice, you got " + userScore + " correct";
-//   testscreen.append(resultEl);
-// }
-if(currentQuestionIndex===questionsArray.length-1){
-  displayResult();
-
-}
-
+    answersElement.setAttribute("class", "answers-i");
+    answersElement.setAttribute("class", "button btn-primary m-5 rounded-pill");
+    answersElement.setAttribute("data-index", i);
+    var quizAnswers = currentQuestion.answers[i];
+    answersElement.textContent = quizAnswers;
+    testscreen.append(answersElement);
+    answersElement.addEventListener("click", function (event) {
+      currentQuestionIndex++;
+      console.log(event.target);
+      displayQuestions();
+      var userchoice = event.target.textContent;
+      if (userchoice === currentQuestion.rightAnswer) {
+        alert("correct");
+        userScore++;
+        results.textContent = "Score: " + userScore;
+        testscreen.append(results);
+      } else {
+        secondsLeft = secondsLeft - 10;
+        alert("incorrect");
+      }
+      console.log(userScore);
+    });
+  }
+  // if(i===currentQuestionIndex){
+  // var resultEl = document.createElement("h4");
+  //   resultEl.textContent = "Nice, you got " + userScore + " correct";
+  //   testscreen.append(resultEl);
+  // }
+  if (currentQuestionIndex === questionsArray.length - 1) {
+    displayResult();
+  }
 }
 startButton.addEventListener("click", function () {
   displayQuestions();
-  startTime()
+  startTime();
 });
